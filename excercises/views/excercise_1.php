30dd0f05b293e1379a3976181fe97dbad50c33a9
jonasdm10 <jonas.de.maesschalck@hotmail.com>
2018-09-15 15:56:20 +0200

    added session

--- excercises/views/excercise_1.php
+++ excercises/views/excercise_1.php
@@ -41,3 +41,40 @@
 <script src="../assets/js/script.js"></script>
 </body>
 </html>
+
+<form action="action_page.php">
+  <div class="imgcontainer">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+<?php
+
+session_save_path('C:\Users\jonas\Desktop\Monkeyproof\mpacademy\excercises');
+session_name('JohnDoe');
+session_id(1);
+
+session_start();  
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+?>
